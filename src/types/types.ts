05b9f0f9b26fcad7de6d46ffefa1e04e7d0a02de
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2024-01-07 20:18:49 -0300

    Adding API.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,22 @@
 import { Product } from "@/API";
 
+export interface TableValues {
+  name?: string | null | undefined;
+  value?: string | null | undefined;
+  createdAt: string;
+}
+
+export interface ProductDetailsProps {
+  headingName: string;
+  items: TableValues[];
+}
+
+export interface ProductsTableProps {
+  products: Product[];
+  onClickDelete: (id: string) => void;
+  admin: boolean;
+}
+
 export interface ItemsTableProps {
   tableName: string;
   data: TableValues[];
